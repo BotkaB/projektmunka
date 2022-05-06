dc18a739ca1d0cd705ed916a18095fded3c085a2
BotkaB <botka.bianka@diak.szamalk-szalezi.hu>
2022-05-06 16:08:23 +0200

    kiír javítva

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
 window.addEventListener('load', init)
 function $(elem) {
   return document.querySelectorAll(elem)
-  
+}
+function ID(elem) {
+  return document.getElementById(elem);
+}
+function CLASS(elem) {
+  return document.getElementsByClassName(elem);
 }
 
-function init(){
+function init() {
   fetch("kerdes.json")
     .then((response) => response.json())
     .then((data) => {
       console.log(data.kerdesek)
       feldolgoz(data.kerdesek)
     })
+  fetch("cikk.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.cikkek)
+      feldolgoz2(data.cikkek)
+    })
+}
+function feldolgoz(kerdesek) {
+  var txt = ''
+  kerdesek.forEach(function (kerdes) {
+
+    txt += '<ul>'
+    for (const key in kerdes) {
+      txt += `<li><span>${key}:</span><span> ${kerdes[key]}</span></li>`
+
     }
-    function feldolgoz(kerdesek) {
-        var txt = ''
-        kerdesek.forEach(function (kerdes) {
-          
-          txt += '<ul>'
-          for (const key in kerdes) {
-            txt += `<li><span>${key}:</span><span> ${kerdes[key]}</span></li>`
-            // txt += "<li><span>"+key+":</span><span> "+kutya[key]+"</span></li>"
-          }
-       
-          txt += '</ul>'
-        })
+
+    txt += '</ul>'
+  })
+
+  console.log(txt);
+  CLASS('kerdesek')[0].innerHTML = txt;
+}
+
+function feldolgoz2(cikkek) {
+  var txt2 = ''
+  cikkek.forEach(function (cikk) {
+
+    txt2 += '<ul>'
+    for (const key in cikk) {
+      txt2 += `<li><span>${key}:</span><span> ${cikk[key]}</span></li>`
       
-        console.log(txt)
-        $('article')[0].innerHTML = txt
-      }
-      
-      function init(){
-        fetch("cikkek.json")
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data.cikkek)
-            feldolgoz(data.cikkek)
-          })
-          }
-          function feldolgoz(cikkek) {
-              var txt = ''
-              cikkek.forEach(function (cikkek) {
-                
-                txt += '<ul>'
-                for (const key in cikkek) {
-                  txt += `<li><span>${key}:</span><span> ${cikkek[key]}</span></li>`
-                  // txt += "<li><span>"+key+":</span><span> "+kutya[key]+"</span></li>"
-                }
-             
-                txt += '</ul>'
-              })
-            
-              console.log(txt)
-              $('article')[0].innerHTML = txt
-            }
+    }
+
+    txt2 += '</ul>'
+  })
+
+  console.log(txt2);
+  CLASS('cikkek')[0].innerHTML = txt2;
+}
