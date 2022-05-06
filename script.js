68f040caf3a84c356a7818cb6aeeef1950627959
davidszabo192 <szabo.david@diak.szamalk-szalezi.hu>
2022-05-06 13:36:10 +0200

    hali

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', init)
 function $(elem) {
   return document.querySelectorAll(elem)
+  
 }
 
 
@@ -29,3 +30,27 @@ function init(){
         $('article')[0].innerHTML = txt
       }
       
+      function init(){
+        fetch("cikkek.json")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.cikkek)
+            feldolgoz(data.cikkek)
+          })
+          }
+          function feldolgoz(cikkek) {
+              var txt = ''
+              cikkek.forEach(function (cikkek) {
+                
+                txt += '<ul>'
+                for (const key in cikkek) {
+                  txt += `<li><span>${key}:</span><span> ${cikkek[key]}</span></li>`
+                  // txt += "<li><span>"+key+":</span><span> "+kutya[key]+"</span></li>"
+                }
+             
+                txt += '</ul>'
+              })
+            
+              console.log(txt)
+              $('article')[0].innerHTML = txt
+            }
